d1f7f53765a8a179e586eec8447e672cb02f6c5f
Deni Spasovski <denimf@gmail.com>
2019-06-09 23:14:13 -0400

    added default routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AssetsComponent } from './pages/assets/assets.component';
+import { DetailsComponent } from './pages/details/details.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'assets', component: AssetsComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'details/:symbol', component: DetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, AssetsComponent, DetailsComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
