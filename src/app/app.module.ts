b7919fa838d31d566a1cd13da7468abb46141f4c
Deni Spasovski <denimf@gmail.com>
2019-06-09 23:14:13 -0400

    initialized componets and added default routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AssetsComponent } from './pages/assets/assets.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent, 
+    AssetsComponent, 
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
