40ae1389c049869bbe20e4a6cc408809b8e573a3
Deni Spasovski <denimf@gmail.com>
2019-06-11 01:35:44 -0400

    added fetching for user assets
    (cherry picked from commit f8f23f83264242aa116040129e1180cde04d43fb)

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,73 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { map as rxMap, filter as rxFilter } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private allocations;
+  private allocationsSubscription = new Subject<any>();
   get watchListUrl() {
     return environment.serverUrl + 'userdata/watchlist';
   }
 
+  get allocationsUrl() {
+    return environment.serverUrl + 'userdata/allocations';
+  }
+
   constructor(private http: HttpClient) {}
+
+  /**
+   * The pattern which we follow here is to get the data from server only once and share subscription for future updates
+   *
+   * @returns
+   * @memberof UserService
+   */
+  getAllocations(): { data: Array<any>; subscription: Subject<any> } {
+    if (!this.allocations) {
+      this.allocations = [];
+      this.http.get(this.allocationsUrl).subscribe((data) => {
+        this.allocations = data;
+        this.allocationsSubscription.next({
+          isInit: true,
+          data: this.allocations
+        });
+      });
+    }
+
+    return {
+      subscription: this.allocationsSubscription,
+      data: this.allocations
+    };
+  }
+
+  /**
+   * This method shares the same subscription as getting all assets and adds pipe to only return data for a single asset
+   *
+   * @param {*} symbol
+   * @returns
+   * @memberof UserService
+   */
+  getAllocationsForAsset(symbol): { data: any; subscription: Observable<any> } {
+    let subscription = this.getAllocations().subscription.pipe(
+      rxFilter((response: any) => {
+        return response.isInit || response.symbol === symbol;
+      }),
+      rxMap((result) => this.mapSingleAssetFromList(result.data, symbol))
+    );
+
+    return {
+      subscription: subscription,
+      data: this.mapSingleAssetFromList(this.allocations, symbol)
+    };
+  }
+
+  mapSingleAssetFromList(list, symbol) {
+    return list.find((x) => x.symbol === symbol);
+  }
 
   getWatchList(): Observable<Array<any>> {
     return this.http.get(this.watchListUrl) as Observable<Array<any>>;
@@ -45,5 +101,5 @@ export class UserService {
 
   getUserId() {
     return localStorage['userId'];
-  }
+}
 }
